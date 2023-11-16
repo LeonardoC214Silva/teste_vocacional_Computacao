a3c6e554a1579fabbba2b50ae7e5cbf2b6d2430e
leo <leonardocsilva214@gmail.com>
2023-11-15 20:53:22 -0400

    primeira versao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Questionnaire2 from './Questionnaire2';
+import {useState} from "react";
 function App() {
+    const [selectedProfession, setSelectedProfession] = useState('');
+
+    const handleProfessionSelection = (profession) => {
+        setSelectedProfession(profession);
+    };
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p className="Title1">Teste vocacional de informatica</p>
+          <Questionnaire2 onProfessionSelect={handleProfessionSelection} />
+
+
+
+
       </header>
     </div>
-  );
+
+          );
 }
 
 export default App;
